bc801814b949a2c574f3d2df2878337e9238aac2
g-vernon <github@gvernon.com>
2023-05-12 23:55:51 +0100

    day one solutions

--- 01_Day_JavaScript_Refresher/introduction.js
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -1,1 +1,8 @@
 console.log('Welcome to 30DaysofJavascript!');
+const rectangle = {
+    width: 20,
+    height: 10,
+  }
+  
+  let { width, height, perimeter = 200 } = rectangle
+  console.log(width, height, perimeter) // 20, 10, undefined
